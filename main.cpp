8e33b45c0fc496936974788167b7ab50f09bb794
ikhsaneff <88372794+ikhsaneff@users.noreply.github.com>
2022-10-10 14:42:30 +0700

    Create main.cpp
    Create base for the code

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+
+    string inputEquation;
+    float result;
+
+    cout << "Enter the equation: ";
+    cin >> inputEquation;
+    cout << "Result: " << result;
+
+}
